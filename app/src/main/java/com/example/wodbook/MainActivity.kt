332d6af3c5616d88b320f131bfc60c4ed12f3f5e
Makcime <marlier.maxime@gmail.com>
2023-12-30 17:44:03 +0100

    [add] simple email/password login functionnality with firebase

--- app/src/main/java/com/example/wodbook/MainActivity.kt
+++ app/src/main/java/com/example/wodbook/MainActivity.kt
@@ -1,6 +1,14 @@
 package com.example.wodbook
 
+import android.content.ContentValues
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,36 +19,45 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.wodbook.ui.theme.WodBookTheme
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.auth
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var buttonLogout: Button
+    private lateinit var textView : TextView
+    private lateinit var user : FirebaseUser
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            WodBookTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
+        buttonLogout = findViewById(R.id.btn_logout)
+        textView = findViewById(R.id.user_details)
+
+        val user = auth.currentUser
+
+        if (user == null) {
+            // No user is signed in
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        } else {
+            textView.setText(user.email)
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        buttonLogout.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WodBookTheme {
-        Greeting("Android")
+            // go back to login activity
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
