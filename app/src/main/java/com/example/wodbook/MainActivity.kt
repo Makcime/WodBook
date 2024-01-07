5c49c72965a5d21befe5a0ea5f0b0f6a442d9565
Makcime <marlier.maxime@gmail.com>
2024-01-07 16:31:31 +0100

    [fix] reload wods on resume of main activity

--- app/src/main/java/com/example/wodbook/MainActivity.kt
+++ app/src/main/java/com/example/wodbook/MainActivity.kt
@@ -34,9 +34,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         initializeUI()
         checkAndRequestPermission()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        loadWods() // Refresh the list of WODs
     }
 
     private fun initializeUI() {
